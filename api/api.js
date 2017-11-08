2911bcabcf9f00f3dd9c24c07be105b70eb9a230
Prestat Dimitri <di.prestat@gmail.com>
2017-11-08 18:32:15 +0100

    Ajout de Services
    Ajout d'utilisateur à un projet et Authentification

--- api/api.js
+++ api/api.js
@@ -4,6 +4,36 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var userTest = new Object();
+userTest.id = 1;
+userTest.name = "Prestat";
+userTest.surname = "Dimitri";
+userTest.login = "dp33";
+userTest.password = "GL";
+userTest.projects = [];
+userTest.projects[0] = new Object();
+userTest.projects[0].id = 1;
+userTest.projects[0].name = "bepp";
+userTest.projects[0].description = "Ceci est un projet de gestion de projet.";
+var userTestJSON = JSON.stringify(userTest);
+
+var projectTest = new Object();
+projectTest.id = 1;
+projectTest.name = "bepp";
+projectTest.description = "Ceci est un projet de gestion de projet.";
+projectTest.users = [];
+projectTest.users[0] = new Object();
+projectTest.users[0].id = 1;
+projectTest.users[0].name = "Prestat";
+projectTest.users[0].surname = "Dimitri";
+projectTest.users[0].login = "dp33";
+projectTest.users[0].password = "GL";
+var projectTestJSON = JSON.stringify(projectTest);
+
+//2 Possibilities : From Project add a User or from User add a Projet
+//Here from Project add User
+
 //Add a User Service
 //Suppose :
 // POST : {"name":"foo", "surname":"bar", "login":"user", "password":"pwd"}
@@ -30,6 +60,16 @@ app.post('/api/project/post', function(req, res) {
     //Insersion dans la BD
 });
 
+//Authentification Service
+//Check Login Password
+app.get('/api/user/get/', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    var login = req.params.login;
+    var password = req.params.password;
+    //Check if there is the login / password in the DB.
+    
+});
+
 //Get a User Service
 //From the Login
 app.get('/api/user/get/:login', function(req, res) {
@@ -37,18 +77,7 @@ app.get('/api/user/get/:login', function(req, res) {
     var login = req.params.login;
     //Remplacer userTestJSON par une requête MangoDB qui sélectionne un user selon son login
 
-    var userTest = new Object();
-    userTest.id = 1;
-    userTest.name = "Prestat";
-    userTest.surname = "Dimitri";
-    userTest.login = "dp33";
-    userTest.password = "GL";
-    userTest.projects = [];
-    userTest.projects[0] = new Object();
-    userTest.projects[0].id = 1;
-    userTest.projects[0].name = "bepp";
-    userTest.projects[0].description = "Ceci est un projet de gestion de projet.";
-    var userTestJSON = JSON.stringify(userTest);
+
 
     res.send(userTestJSON);
 });
@@ -60,20 +89,21 @@ app.get('/api/project/get/:name', function(req, res) {
     var name = req.params.name;
     //Remplacer projectTestJSON par une requête MangoDB qui sélectionne un project selon son name
 
-    var projectTest = new Object();
-    projectTest.id = 1;
-    projectTest.name = "bepp";
-    projectTest.description = "Ceci est un projet de gestion de projet.";
-    projectTest.users = [];
-    projectTest.users[0] = new Object();
-    projectTest.users[0].id = 1;
-    projectTest.users[0].name = "Prestat";
-    projectTest.users[0].surname = "Dimitri";
-    projectTest.users[0].login = "dp33";
-    projectTest.users[0].password = "GL";
-    var projectTestJSON = JSON.stringify(projectTest);
 
     res.send(projectTestJSON);
+});
+
+//Add a Project Service
+//Add to the project with the "name" the User with the "login".
+//Suppose :
+// POST : {"name":"foo", "login":"bar"}
+// POST : url?name=foo&login=bar
+app.post('/api/project/post/adduser', function(req, res) {
+    var project = new Object();
+    project.name = req.body.name;
+    project.surname = req.body.description;
+    console.log(project);
+    //Insersion dans la BD
 });
 
 app.use(function(req, res, next){
