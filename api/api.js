613cefce28eb65e497ee0e8bdacf6805823076e7
Prestat Dimitri <di.prestat@gmail.com>
2017-11-08 16:12:46 +0100

    Initialisation API
    Initialisation du Node.js, installation des modules de bases, installation d'express.

---
+++ api/api.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.use(function(req, res, next){
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(404).send('Page introuvable !');
+});
+
+app.listen(8080);
