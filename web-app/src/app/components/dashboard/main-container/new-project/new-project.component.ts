6627a19cc49200b66b180c066dc070d8de614149
Adrien Bounader <adrien.bounader@etu.u-bordeaux.fr>
2017-11-17 00:02:43 +0100

    commentaires et nettoyage

--- web-app/src/app/components/dashboard/main-container/new-project/new-project.component.ts
+++ web-app/src/app/components/dashboard/main-container/new-project/new-project.component.ts
@@ -6,6 +6,9 @@ import {AppConstants} from "../../../../app-constants";
 import {CheckAuthService} from "../../../../services/check-auth.service";
 import {ApiCallingObserverService} from "../../../../services/api-call-observer.service";
 
+/**
+ * Logic of page which contains form for project creation.
+ */
 @Component({
   selector: 'app-new-project',
   templateUrl: './new-project.component.html',
@@ -13,17 +16,44 @@ import {ApiCallingObserverService} from "../../../../services/api-call-observer.
 })
 export class NewProjectComponent implements OnInit {
 
+    /**
+     * FormGroup of form for project creation
+     * @type {FormGroup}
+     */
     private newProjectForm: FormGroup;
 
+    /**
+     * To true if api service for creation has been called and response is not received.
+     * @type {boolean}
+     */
     private newProjectLoading: boolean;
+
+    /**
+     * True if project has been submit at least one time.
+     */
     private newProjectSubmitted: boolean;
+
+    /**
+     * Error message to show to the user.
+     */
     private newProjectMessage: string;
 
+    /**
+     * NewProjectComponent constructor.
+     * @param {HttpClient} httpClient
+     * @param {CheckAuthService} checkAuthService
+     * @param {Router} router
+     * @param {ApiCallingObserverService} apiCallingObserverService
+     */
     public constructor(private httpClient: HttpClient,
                        private checkAuthService: CheckAuthService,
                        private router: Router,
                        private apiCallingObserverService: ApiCallingObserverService) { }
 
+    /**
+     * Initialize newProjectForm variable.
+     * The FormGroup
+     */
     public ngOnInit(): void {
         this.newProjectSubmitted = false;
         this.newProjectLoading = false;
@@ -38,15 +68,23 @@ export class NewProjectComponent implements OnInit {
         })
     }
 
+    /**
+     * Return FormControl named "name"
+     * @return {AbstractControl}
+     */
     public get name (): AbstractControl {
         return this.newProjectForm.get ('name');
     }
 
+    /**
+     * If FormGroup is valid and not called yet, we call api service to create project.
+     */
     public newProjectSubmit () {
         this.newProjectSubmitted = true;
         if (this.newProjectForm.valid && !this.newProjectLoading) {
             this.newProjectLoading = true;
 
+            // join token to parameters
             const body = Object.assign ({},
                 this.newProjectForm.value,
                 {
@@ -64,7 +102,6 @@ export class NewProjectComponent implements OnInit {
                 this.router.navigate(['dashboard/projects', this.newProjectForm.value.name])
 
             }, (error) => {
-                console.log (error);
                 this.newProjectLoading = false;
 
                 this.checkAuthService.check(error);
@@ -75,5 +112,4 @@ export class NewProjectComponent implements OnInit {
             });
         }
     }
-
 }
--- web-app/src/app/components/dashboard/main-container/project-container/overview-container/overview-container.component.ts
+++ web-app/src/app/components/dashboard/main-container/project-container/overview-container/overview-container.component.ts
@@ -1,44 +1,91 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppConstants} from "../../../../../app-constants";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
 import {Subscription} from "rxjs/Subscription";
 import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
-import {ApiCallingObserverService} from "../../../../../services/api-call-observer.service";
 import {CheckAuthService} from "../../../../../services/check-auth.service";
 
+/**
+ * Logic for the overview page of one project.
+ */
 @Component({
     templateUrl: './overview-container.component.html',
     styleUrls: ['./overview-container.component.css']
 })
 export class OverviewContainerComponent implements OnInit {
 
-    showAddMember = false;
+    /**
+     * If true, the form for adding member is shown.
+     * @type {boolean}
+     */
+    private showAddMember: boolean;
 
-    private loading: boolean;
+    /**
+     * True if the GET project has been called but not received.
+     * @type {boolean}
+     */
+    private overviewLoading: boolean;
+
+    /**
+     * Received data from api, project to display.
+     */
     private currentProject: any;
+
+    /**
+     * Subscription for route params, to delete it in ngOnDestroy.
+     */
     private routeParamsSub: Subscription;
 
+    /**
+     * FormGroup for add member form
+     */
     private addMemberForm: FormGroup;
+
+    /**
+     * True if adding member has been submit at least one time.
+     */
     private addMemberFormSubmitted: boolean;
+
+    /**
+     * True if adding member has been submit, and response not received.
+     */
     private addMemberFormLoading: boolean;
 
+    /**
+     * Error to show for add memeber form.
+     */
     private errorMessageAddMember: string;
 
+    /**
+     * Option of select for role (add member form)
+     * @type {{id: number, name: string}[]}
+     */
     private availabledRole = [
         {id: 0, name: "Développeur"},
         {id: 1, name: "Product Owner"}
     ];
 
+    /**
+     * OverviewContainerComponent constructor.
+     * @param {HttpClient} httpClient
+     * @param {ActivatedRoute} activatedRoute
+     * @param {CheckAuthService} checkAuthService
+     */
     public constructor(private httpClient: HttpClient,
                        private activatedRoute: ActivatedRoute,
                        private checkAuthService: CheckAuthService) {
-        this.loading = true;
+        this.overviewLoading = true;
         this.errorMessageAddMember = null;
+        this.showAddMember = false;
     }
 
+    /**
+     * Initialize form variable and GroupForm.
+     * subscribe for route parameters to.
+     */
     public ngOnInit(): void {
-        this.loading = true;
+        this.overviewLoading = true;
 
         this.addMemberFormSubmitted = false;
         this.addMemberFormLoading = false;
@@ -58,6 +105,10 @@ export class OverviewContainerComponent implements OnInit {
         });
     }
 
+    /**
+     * Call api GET /api/project/:name service for requested project.
+     * @param {string} projectName
+     */
     private getProject (projectName: string) {
         let httpParams = new HttpParams()
             .set('token', localStorage.getItem(AppConstants.ACCESS_COOKIE_NAME));
@@ -68,24 +119,32 @@ export class OverviewContainerComponent implements OnInit {
             params: httpParams,
             responseType: 'json'
         }).subscribe((response: any) => {
-            this.loading = false;
+            this.overviewLoading = false;
             this.currentProject = response[0];
         }, (error) => {
-            this.loading = false;
+            this.overviewLoading = false;
             this.checkAuthService.check(error);
         });
     }
 
+    /**
+     * Unsuscribe for route params changing
+     */
     public ngOnDestroy (): void {
         this.routeParamsSub.unsubscribe();
         this.routeParamsSub = null;
     }
 
-    toggleAddMember() {
-        //TODO: get members from the API
+    /**
+     * Toggle addMember form
+     */
+    public toggleAddMember(): void {
         this.showAddMember = !this.showAddMember;
     }
 
+    /**
+     * Submit the form adding if filled values are valid.
+     */
     public submitAddMemberForm(): void {
         this.addMemberFormSubmitted = true;
 
@@ -109,10 +168,12 @@ export class OverviewContainerComponent implements OnInit {
             ).subscribe((response: any) => {
                 this.addMemberFormLoading = false;
                 this.getProject (this.currentProject.name);
-                this.showAddMember = !this.showAddMember;
                 this.errorMessageAddMember = null;
                 this.login.setValue(null);
                 this.addMemberFormSubmitted = false;
+
+                this.toggleAddMember();
+
             }, (error) => {
                 this.addMemberFormLoading = false;
 
@@ -125,8 +186,11 @@ export class OverviewContainerComponent implements OnInit {
         }
     }
 
+    /**
+     * Return FormControl login.
+     * @return {AbstractControl}
+     */
     public get login (): AbstractControl {
         return this.addMemberForm.get('login');
     }
-
 }
