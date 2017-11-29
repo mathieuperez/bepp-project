f7567108f006e0774ce4cb2a4c5e585f86bb770b
mathieuperez <mathieuperez3@gmail.com>
2017-11-29 20:53:39 +0100

    correction routage

--- api/api.js
+++ api/api.js
@@ -11,7 +11,7 @@ const users = require('./users');
 const projects = require('./projects');
 const userStories = require('./userStories');
 
-var app = express.Router();
+var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
